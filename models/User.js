026a827f0f7915b140001680a070ccf1b886adbc
zina-hub <yo.owca@gmail.com>
2018-11-21 14:03:50 +0100

    registration

--- models/User.js
+++ models/User.js
@@ -13,12 +13,7 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
-    },
-    avatar: {
-        type: String,
-        required: true
-    },
-     
+    },    
 });
 
 module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../../models/User');
+
+
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
+// @route   GET api/users/register
+// @desc    Registers the user
+// @access  Public
+router.post('/register', (req, res) => {
+    // check if user exists
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'Email already exists in the database' });
+            }
+            else {
+                const newUser = new User(
+                    {
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password,
+                    }
+                );
+
+                // password hashing
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
+})
 module.exports = router;
