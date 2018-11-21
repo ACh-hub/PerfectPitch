1140d25008114d6cba11c1b3774128b2aa7c6839
zina-hub <yo.owca@gmail.com>
2018-11-21 14:24:32 +0100

    No token generation yet

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,4 +44,31 @@ router.post('/register', (req, res) => {
             }
         })
 })
+
+// @route   GET api/users/login
+// @desc    Returning JWT token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'User not found' });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        // generate JWT token
+                        res.json({ msg: 'Success' });
+                    } else {
+                        return res.status(400).json({ password: 'Password incorrect' });
+                    }
+                });
+        })
+
+});
+
 module.exports = router;
